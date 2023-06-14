1fe7b65b021e8bbc6240983dda99cdf96770777b
tonito98 <86890715+tonito98@users.noreply.github.com>
2023-06-14 15:29:02 +0200

    friend list appears next to the user's thoughts

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from   'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import SignUp from './pages/Signup';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -19,13 +25,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+    <Router>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
-        <Home />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/profile/:username?' component={Profile} />
+          <Route exact path='/thought/:id' component={SingleThought} />
+
+          <Route component={NoMatch} />
+        </Switch>
       </div>
       <Footer />
     </div>
+    </Router>
     </ApolloProvider>
   );
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,44 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_USER =gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            friendCount
+            friends {
+                _id
+                username
+            }
+            thoughts {
+                _id
+                thoughtText
+                createdAt
+                reactionCount
+            }
+        }
+    }
+    `;
+    
+export const QUERY_THOUGHT = gql`
+    query thought($id: ID!){
+        thought(_id: $id){
+            _id 
+            thoughtText
+            createdAt
+            username
+            reactionCount
+            reactions {
+                _id
+                createdAt
+                username
+                reactionBody
+            }
+        }
+    }
+    `;
+
 export const QUERY_THOUGHTS = gql`
     query thoughts($username: String){
         thoughts(username: $username){
