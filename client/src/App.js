d7aa78c88baa68591af0a4b4196c15e178fb4f19
tonito98 <86890715+tonito98@users.noreply.github.com>
2023-06-18 16:11:21 +0200

    user can create account and logint through the font end

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,25 @@ import Profile from './pages/Profile';
 import SignUp from './pages/Signup';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return{
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation, userMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
-
+  const [addUser, { error }] = useMutation(ADD_USER);
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -13,10 +16,22 @@ const Signup = () => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
+  // submit form (notice the async!)
+  const handleFormSubmit = async event => {
     event.preventDefault();
-  };
+    // use try/catch instead of promises to handle errors
+    try {
+      // execute addUser mutation and pass in variable data from the form
+      const { data } = await addUser({
+        variables: {...formState}
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+    }
+  
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -56,6 +71,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
